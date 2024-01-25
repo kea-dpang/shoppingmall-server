4a2a7646a5e6a5f77f09fece6c01bb3d15cdf4c7
asasia1935 <sungmin1935@naver.com>
2024-01-25 16:21:18 +0900

    [Remove] 주문 및 마일리지 같은 새로 서비스로 만든 엔티티 및 자료 삭제

--- src/main/java/com/example/shoppingmallserver/entity/order/RefundStatus.java
+++
@@ -1,7 +0,0 @@
-package com.example.shoppingmallserver.entity.order;
-
-public enum RefundStatus {
-    REFUND_REQUEST,
-    RECOVERING,
-    REFUND_COMPLETE
-}
--- src/main/java/com/example/shoppingmallserver/repository/MileageRepository.java
+++
@@ -1,8 +0,0 @@
-package com.example.shoppingmallserver.repository;
-
-import com.example.shoppingmallserver.entity.mileage.Mileage;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface MileageRepository extends JpaRepository<Mileage, Long> {
-}
