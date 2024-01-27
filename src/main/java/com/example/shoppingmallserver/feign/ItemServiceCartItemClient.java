bfd6769b09ae76267f7b8bf7830dd89a846dc69a
asasia1935 <sungmin1935@naver.com>
2024-01-27 18:26:56 +0900

    [Refactor] feign client 타겟 이름 변경

--- src/main/java/com/example/shoppingmallserver/feign/ItemServiceCartItemClient.java
+++ src/main/java/com/example/shoppingmallserver/feign/ItemServiceCartItemClient.java
@@ -1,6 +1,5 @@
 package com.example.shoppingmallserver.feign;
 
-import com.example.shoppingmallserver.dto.ReadItemsDto;
 import com.example.shoppingmallserver.dto.ReadItemsInfoDto;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -9,7 +8,7 @@ import org.springframework.web.bind.annotation.RequestBody;
 import java.util.List;
 
 // 자바독 주석 나중에 추가
-@FeignClient(name = "item-service")
+@FeignClient(name = "item-server")
 public interface ItemServiceCartItemClient {
 
     // 장바구니 목록 조회를 위한 정보 요청
