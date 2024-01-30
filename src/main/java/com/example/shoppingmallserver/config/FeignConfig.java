223a78f37994842b716d3d0593ff7c1055d6f1eb
SeungHyeon <namsh1125@naver.com>
2024-01-31 01:01:38 +0900

    Update: Feign Client에 RequestInterceptor 정의

--- src/main/java/com/example/shoppingmallserver/config/FeignConfig.java
+++ src/main/java/com/example/shoppingmallserver/config/FeignConfig.java
@@ -1,13 +1,30 @@
 package com.example.shoppingmallserver.config;
 
 import feign.Logger;
+import feign.RequestInterceptor;
+import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.context.request.RequestContextHolder;
+import org.springframework.web.context.request.ServletRequestAttributes;
 
 @Configuration
 public class FeignConfig {
     @Bean
     Logger.Level feignLoggerLevel() {
         return Logger.Level.FULL;
+    }
+
+    @Bean
+    public RequestInterceptor requestInterceptor() {
+        return requestTemplate -> {
+            ServletRequestAttributes attributes = (ServletRequestAttributes) RequestContextHolder
+                    .getRequestAttributes();
+            if (attributes == null) {
+                return;
+            }
+            HttpServletRequest request = attributes.getRequest();
+            requestTemplate.header("X-DPANG-CLIENT-ID", request.getHeader("X-DPANG-CLIENT-ID"));
+        };
     }
 }
