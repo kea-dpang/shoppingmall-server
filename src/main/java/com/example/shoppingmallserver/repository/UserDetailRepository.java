67a17081a44516792e6b978c565f0c8d5661a11a
asasia1935 <88706217+asasia1935@users.noreply.github.com>
2024-02-01 09:49:45 +0900

    [Fix] 유저와 유저 정보만 지연로딩 하도록 쿼리 수정 (#56)

--- src/main/java/com/example/shoppingmallserver/repository/UserDetailRepository.java
+++ src/main/java/com/example/shoppingmallserver/repository/UserDetailRepository.java
@@ -1,10 +1,13 @@
 package com.example.shoppingmallserver.repository;
 
+import com.example.shoppingmallserver.entity.user.User;
 import com.example.shoppingmallserver.entity.user.UserDetail;
 
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import java.util.List;
 
@@ -14,20 +17,38 @@ import java.util.List;
  */
 public interface UserDetailRepository extends JpaRepository<UserDetail, Long> {
 
+    /**
+     * 사용자의 사원 번호에 특정 키워드가 포함된 사용자의 상세 정보 목록을 페이지네이션하여 조회합니다.
+     *
+     * @param keyword  사원 번호에서 검색할 키워드
+     * @param pageable 페이지네이션 정보
+     * @return 키워드가 포함된 사원 번호를 가진 사용자의 상세 정보 목록
+     */
     Page<UserDetail> findByEmployeeNumberContaining(String keyword, Pageable pageable);
 
     /**
-     * 사용자 이름에 특정 키워드가 포함된 사용자의 상세 정보 목록을 조회합니다.
-     * 이 메서드는 주로 관리자가 사용자 정보를 검색할 때 사용됩니다.
+     * 사용자의 이름에 특정 키워드가 포함된 사용자의 상세 정보 목록을 페이지네이션하여 조회합니다.
      *
      * @param keyword  사용자 이름에서 검색할 키워드
-     * @param pageable
+     * @param pageable 페이지네이션 정보
      * @return 키워드가 포함된 사용자 이름을 가진 사용자의 상세 정보 목록
      */
     Page<UserDetail> findByNameContaining(String keyword, Pageable pageable);
 
+    /**
+     * 사용자의 가입 날짜에 특정 키워드가 포함된 사용자의 상세 정보 목록을 페이지네이션하여 조회합니다.
+     *
+     * @param keyword  가입 날짜에서 검색할 키워드
+     * @param pageable 페이지네이션 정보
+     * @return 키워드가 포함된 가입 날짜를 가진 사용자의 상세 정보 목록
+     */
     Page<UserDetail> findByJoinDateContaining(String keyword, Pageable pageable);
 
-
+    /**
+     * 사용자 ID에 해당하는 사용자의 상세 정보를 조회합니다.
+     *
+     * @param userId 조회할 사용자의 ID
+     * @return 사용자 ID에 해당하는 사용자의 상세 정보
+     */
     UserDetail findByUserId(Long userId);
 }
