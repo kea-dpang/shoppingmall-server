7135d6ef5233f5f6ff2ab58d03de49db3b9a8cc8
asasia1935 <sungmin1935@naver.com>
2024-02-07 03:00:59 +0900

    [Refactoring ] Update Dto -> 개인정보 마스킹 작업

--- src/main/java/com/example/shoppingmallserver/dto/response/user/AdminReadUserResponseDto.java
+++ src/main/java/com/example/shoppingmallserver/dto/response/user/AdminReadUserResponseDto.java
@@ -28,10 +28,32 @@ public class AdminReadUserResponseDto {
         this.userId = userDetail.getUser().getId();
         this.employeeNumber = userDetail.getEmployeeNumber();
         this.name = userDetail.getName();
-        this.email = user.getEmail();
+        this.email = maskEmail(user.getEmail());
         this.joinDate = userDetail.getJoinDate();
-        this.defaultAddress = userDetail.getZipCode() + " "
-                + userDetail.getAddress() + " " +
-                userDetail.getDetailAddress();
+        this.defaultAddress = maskAddress(userDetail.getAddress());
     }
+
+    /**
+     * 이메일 주소를 마스킹 처리합니다.
+     *
+     * @param email 마스킹 처리할 이메일 주소
+     * @return 마스킹 처리된 이메일 주소
+     */
+    private String maskEmail(String email) {
+        int index = email.indexOf("@");
+        if (index <= 1) {
+            return email;
+        } else {
+            return email.charAt(0) + email.substring(1, index).replaceAll("\\.", "*") + email.substring(index);
+        }
+    }
+
+    private String maskAddress(String address) {
+        String maskedZipCode = "****";
+        String[] addressParts = address.split(" ", 3);
+        String maskedAddress = addressParts[0] + " " + addressParts[1] + " ****";
+        return maskedZipCode + " " + maskedAddress;
+    }
+
+
 }
