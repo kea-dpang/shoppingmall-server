2cda70e1c20546852df2c4e821acb435b70c5c69
asasia1935 <sungmin1935@naver.com>
2024-02-08 15:52:54 +0900

    [Fix] 관리자의 사용자 삭제 수정(미완성)
    사용자가 삭제될 때 다른 서비스에 요청해서 정보 삭제하도록 변경
    주문 서비스는 다시 한번 체크해서 수정하도록 함

--- src/main/java/com/example/shoppingmallserver/service/UserServiceImpl.java
+++ src/main/java/com/example/shoppingmallserver/service/UserServiceImpl.java
@@ -5,7 +5,10 @@ import com.example.shoppingmallserver.entity.cart.Cart;
 import com.example.shoppingmallserver.entity.user.*;
 import com.example.shoppingmallserver.entity.wishlist.Wishlist;
 import com.example.shoppingmallserver.exception.*;
+import com.example.shoppingmallserver.feign.auth.AuthFeignClient;
+import com.example.shoppingmallserver.feign.item.ItemFeignClient;
 import com.example.shoppingmallserver.feign.mileage.MileageFeignClient;
+import com.example.shoppingmallserver.feign.order.OrderFeignClient;
 import com.example.shoppingmallserver.repository.*;
 
 import lombok.RequiredArgsConstructor;
@@ -32,10 +35,15 @@ public class UserServiceImpl implements UserService {
     private final WishlistRepository wishlistRepository;
 
     /**
-     * 마일리지와 관련된 기능을 제공하는 Feign 클라이언트입니다.
-     * 이 클라이언트를 사용해 마일리지 서비스와 통신할 수 있습니다.
+     * 마일리지, 상품, 주문, 인증과 관련된 기능을 제공하는 Feign 클라이언트입니다.
+     * 이 클라이언트를 사용해 각 서비스와 통신할 수 있습니다.
      */
     private final MileageFeignClient mileageFeignClient;
+    private final ItemFeignClient itemFeignClient;
+    private final OrderFeignClient orderFeignClient;
+    private final AuthFeignClient authFeignClient;
+
+
 
     @Override
     public void register(String email, Long employeeNumber, String name, LocalDate joinDate) {
@@ -215,8 +223,15 @@ public class UserServiceImpl implements UserService {
             // 사용자 조회, 없을 경우 예외 발생
             User user = userRepository.findById(userId).orElseThrow(() -> new UserNotFoundException(userId));
 
-            // 사용자 삭제
+            // 사용자 삭제(관련된 사용자 서비스 내 데이터 삭제)
             userRepository.delete(user);
+            cartRepository.delete(user.getCart());
+            wishlistRepository.delete(user.getWishlist());
+
+            // 사용자가 남긴 모든 기록 삭제
+            mileageFeignClient.deleteMileage(userId, userId);
+            itemFeignClient.deleteReview(userId);
+            // Todo: orderService 삭제
 
             log.info("사용자 정보 삭제 성공. 삭제된 사용자 아이디: {}", userIds);
         }
